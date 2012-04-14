f5c1e75aa73f1a1603621675cb855835bab6ba1a
Hideaki Takahashi <mymelo@gmail.com>
2012-04-14 17:59:05 +0900

    add Python2 and Python3 compatibility. according to package renaming.

--- test.py
+++ test.py
@@ -1,20 +1,25 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 import os
 from whoosh.index import create_in
 from whoosh.fields import *
 from whoosh.qparser import QueryParser
+import whooshjp
+import whooshjp.Filters
+
 
 def add_docs(w):
-    w.add_document(title=u'その1', path=u'1', content=u'こんにちは世界')
-    w.add_document(title=u'その2', path=u'2', content=u'さようなら世界')
-    w.add_document(title=u'その3', path=u'3', content=u'今日はいい天気')
-    w.add_document(title=u'その4', path=u'4', content=u'これは日本語をテストする文章です。')
+    w.add_document(title='その1', path='1', content='こんにちは世界')
+    w.add_document(title='その2', path='2', content='さようなら世界')
+    w.add_document(title='その3', path='3', content='今日はいい天気')
+    w.add_document(title='その4', path='4', content='これは日本語をテストする文章です。')
     w.commit()
 
 def search(s, qp, text):
-    print 'search ' + text
+    print('search ' + text)
     for r in s.search(qp.parse(text)):
-        print r['path'], r['title']
+        print(r['path'], r['title'])
 
 def test_(tk):
     scm = Schema(title=TEXT(stored=True, analyzer=tk), path=ID(unique=True,stored=True), content=TEXT(stored=True, analyzer=tk))
@@ -27,42 +32,53 @@ def test_(tk):
     s = ix.searcher()
     qp = QueryParser("content", schema=ix.schema)
 
-    search(s, qp, u"こんにちは世界")
-    search(s, qp, u"世界")
-    search(s, qp, u"こんにちは")
-    search(s, qp, u"さようなら")
-    search(s, qp, u"今日はいい天気")
-    search(s, qp, u"天気")
-    search(s, qp, u"は")
+    search(s, qp, "こんにちは世界")
+    search(s, qp, "世界")
+    search(s, qp, "こんにちは")
+    search(s, qp, "さようなら")
+    search(s, qp, "今日はいい天気")
+    search(s, qp, "天気")
+    search(s, qp, "は")
     ix.close()
- 
+
 def test_tokenize(tk):
-    for i in tk(u'今日はいい天気'):
-        print i.text, i.stopped
-    for i in tk(u'これは日本語をテストする文章です。', positions=True, chars=True):
-        print i.text, i.pos, i.startchar, i.endchar
+    for i in tk('今日はいい天気'):
+        print(i.text, i.stopped)
+    for i in tk('これは日本語をテストする文章です。', positions=True, chars=True):
+        print(i.text, i.pos, i.startchar, i.endchar)
 
-import WhooshJapaneseTokenizer
-import WhooshJapaneseTokenizer.Filters
-import igo.Tagger
-tk = WhooshJapaneseTokenizer.IgoTokenizer(dataDir='ipadic', gae=False)
-tk = WhooshJapaneseTokenizer.IgoTokenizer(igo.Tagger.Tagger('ipadic'))
-test_tokenize(tk)
-tk = tk | WhooshJapaneseTokenizer.Filters.FeatureFilter([u'^助詞,係助詞.*$'])
-test_(tk)
-test_tokenize(tk)
+try:
+    print('Igo')
+    from whooshjp.IgoTokenizer import IgoTokenizer
+    import igo.Tagger
+    tk = IgoTokenizer(dataDir='ipadic', gae=False)
+    tk = IgoTokenizer(igo.Tagger.Tagger('ipadic'))
+    test_tokenize(tk)
+    tk = tk | whooshjp.Filters.FeatureFilter(['^助詞,係助詞.*$'])
+    test_(tk)
+    test_tokenize(tk)
+except:
+    print('skip')
 
-import MeCab
-print 'MeCab'
-tk = WhooshJapaneseTokenizer.MeCabTokenizer()
-test_tokenize(tk)
-tk = tk | WhooshJapaneseTokenizer.Filters.FeatureFilter([u'^助詞,係助詞.*$'])
-test_(tk)
-test_tokenize(tk)
+try:
+    print('MeCab')
+    import MeCab
+    from whooshjp.MeCabTokenizer import MeCabTokenizer
+    tk = MeCabTokenizer()
+    test_tokenize(tk)
+    tk = tk | whooshjp.Filters.FeatureFilter(['^助詞,係助詞.*$'])
+    test_(tk)
+    test_tokenize(tk)
+except:
+    print('skip')
 
-
-import tinysegmenter
-tk = WhooshJapaneseTokenizer.TinySegmenterTokenizer(tinysegmenter.TinySegmenter())
-test_(tk)
-test_tokenize(tk)
+try:
+    print('TinySegmenter')
+    import tinysegmenter
+    from whooshjp.TinySegmenterTokenizer import TinySegmenterTokenizer
+    tk = TinySegmenterTokenizer(tinysegmenter.TinySegmenter())
+    test_(tk)
+    test_tokenize(tk)
+except:
+    print('skip')
 
